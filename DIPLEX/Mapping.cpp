afd261c09e43fa95bf9c09d22d4bbd035998ce1e
Draco 420 <109557311+JoeySoprano420@users.noreply.github.com>
2025-03-16 04:51:38 -0400

    Create Mapping.cpp

---
+++ DIPLEX/Mapping.cpp
@@ -0,0 +1,7 @@
+std::unordered_map<std::string, std::string> conversionTable = {
+    {"output", "std::cout"},
+    {"var", "int"},
+    {"fn", "function"},
+    {"start", "block_start"},
+    {"stop", "block_end"}
+};
